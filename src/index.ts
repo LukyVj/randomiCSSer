8f7e6cd0c9ae638a3297bca99bd45da56117a04c
LukyVj <lucas.bonomi@gmail.com>
2022-12-16 10:20:03 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -37,11 +37,8 @@ const randomCSSVariable = ({
   const generate = ({ length, dom }: { length: number; dom: boolean }) => {
     Array.from({ length }).map((_, index) => {
       const VAR_NAME = `--${VARIABLE}-${index}`;
-      const VAR_VALUE =
-        UNIT !== ''
-          ? `"${value(range.min || 1, range.max || 100, range.round || true)}${UNIT}"`
-          : `${value(range.min || 1, range.max || 100, range.round || true)}`;
-      ALL_VARS.push(`{"${VAR_NAME}":${VAR_VALUE}}`);
+      const VAR_VALUE = `${value(range.min || 1, range.max || 100, range.round || true)}${UNIT}`;
+      ALL_VARS.push(`{"${VAR_NAME}":"${VAR_VALUE}"}`);
       if (root && dom) {
         root.style.setProperty(`${VAR_NAME}`, `${VAR_VALUE}`);
       }
